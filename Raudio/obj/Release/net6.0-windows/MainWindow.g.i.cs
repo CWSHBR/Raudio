f09932407a11d59e60ac8d7e529ed9fda8edec91
CWSHBR <39835206+CWSHBR@users.noreply.github.com>
2022-08-31 07:57:11 +0300

    b

--- Raudio/obj/Release/net6.0-windows/MainWindow.g.i.cs
+++ Raudio/obj/Release/net6.0-windows/MainWindow.g.i.cs
@@ -182,7 +182,7 @@ namespace Raudio {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Raudio;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Raudio;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
